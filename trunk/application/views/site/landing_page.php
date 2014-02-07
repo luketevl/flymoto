b7562785c7e271ee603389468f9bc1e794d38c3e
luketevl <luketevl@94322557-4496-4598-b6cc-a7925fc4f445>
2014-02-07 10:46:03 +0000

    landing

---
+++ trunk/application/views/site/landing_page.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>resources/css/style.css" />
+	<style type="text/css">
+	</style>
+</head>
+<body>
+	<header>
+	<section id="logo">
+		<img src="<?php echo base_url(); ?>resources/img/site/logo.png" />
+	</section>
+	<nav id="menu">
+		<a href="#">O que é</a>
+		<a href="#">Como funciona</a>
+		<a href="#">Regulamento</a>
+		<a href="#">Cadastre-se</a>
+	</nav>
+	</header>
+	<section>
+		<img src="<?php echo base_url(); ?>resources/img/site/banner.png" class="full_width"/>
+	</section>
+	<section id="conteudo">
+		
+	</section>
+
+</body>
+</html>
